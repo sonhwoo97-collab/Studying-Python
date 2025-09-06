b2e931827ffd106787c867709b6b3344c1899511
sonhwoo97-collab <sonhwoo97@gmail.com>
2025-09-06 17:52:56 +0900

    제곱 추가

--- main.py
+++ main.py
@@ -15,10 +15,14 @@ class Cal:
     def di(self):
         re=self.first/self.second
         return round(re,3)
+    def pow(self):
+        re=self.first**self.second
+        return re
     def print_a(self):
         print(f'덧셈결과: {self.pl()}')
         print(f'뺄셈결과: {self.mi()}')
         print(f'곱셈결과: {self.mu()}')
         print(f'나눗셈결과: {self.di()}')
-a=Cal(5,7)
+        print(f'제곱결과: {self.pow()}')
+a=Cal(2,3)
 a.print_a()
